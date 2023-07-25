7d5ddea6a130832010866bbcc019b14a0b9fad0e
Mocks <aatienzaf@gmail.com>
2023-07-25 08:46:09 +0200

    Changing App.jsx again

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
         <div id="logo-img">
           <img src={logo} />
         </div>
-        <h1>Learn & Master GitHub Actions for sure</h1>
+        <h1>Learn & Master GitHub Actions for sure!!</h1>
       </header>
       <MainContent />
     </>
